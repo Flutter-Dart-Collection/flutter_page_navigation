75198b72337c10c4aa24a7e3c393d5061dd66cbc
KelvinYou <ykekelvin0220@gmail.com>
2023-04-04 09:09:46 +0800

    first commit

---
+++ lib/core/constants/navigation/navigation_constants.dart
@@ -0,0 +1,7 @@
+class NavigationConstants {
+  static const TEST_VIEW_PATH = '/test';
+  static const DEFAULT = '/';
+
+  static const LOGIN_PATH = "/login";
+  static const SETTINGS_PATH = "/settings";
+}
